c4783b5ebb027d37dfa3bc153f40e6a88ea650d6
Mark Sun <sunyunfei201@gmail.com>
2023-11-26 13:15:34 +0800

    study

--- app/components/Loading/Loading.tsx
+++ app/components/Loading/Loading.tsx
@@ -1,7 +1,6 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import gsap from 'gsap';
-import {bold} from "next/dist/lib/picocolors";
 
 const Loading = () => {
     // Define letters as the string "SOME"
@@ -17,43 +16,59 @@ const Loading = () => {
     useEffect(() => {
 
         // Initialize opacity of the word to 0
-        gsap.set(wordRef.current.children, { autoAlpha: 0});
-        gsap.set([line1.current, line2.current], { autoAlpha: 0, x:50 });
-        gsap.set(diagonalLine.current, { autoAlpha: 0 });
+
+        // @ts-ignore
+        gsap.set(wordRef.current.children, {autoAlpha: 0});
+        gsap.set([line1.current, line2.current], {autoAlpha: 0, x: 50});
+        gsap.set(diagonalLine.current, {autoAlpha: 0});
 
 
         // 创建时间线
-        const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+        const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 
+        // @ts-ignore
         console.log(((window.innerWidth - 200) / letters.length) - wordRef.current.children[0].offsetWidth)
         // Calculate the scale factor based on the screen width and the original width of the letter
-        const marginFactor = Math.max(10,((window.innerWidth - 200) / letters.length) - wordRef.current.children[0].offsetWidth);
-        const scaleFactor = Math.max(1.5, ((window.innerWidth - 400)  / letters.length)  / (wordRef.current.children[0].offsetWidth));
+        // @ts-ignore
+        const marginFactor = Math.max(10, ((window.innerWidth - 200) / letters.length) - wordRef.current.children[0].offsetWidth);
+        // @ts-ignore
+        const scaleFactor = Math.max(1.5, ((window.innerWidth - 400) / letters.length) / (wordRef.current.children[0].offsetWidth));
         // Calculate the margin factor based on the screen width and the number of letters
 
-        tl.to(wordRef.current.children, { autoAlpha: 1, x: 0, duration: 1-0.05*letters.length, stagger: { each: 0.05, from: "end" },ease: 'power3.out',marginRight: marginFactor,scale: scaleFactor,fontWeight: 'bold'});
-        tl.to(wordRef.current.children, {x: 20, duration: 0.5, scale:1,marginRight: '5px',border:0,},'+=.7');
+        // @ts-ignore
+        tl.to(wordRef.current.children, {
+            autoAlpha: 1,
+            x: 0,
+            duration: 1 - 0.05 * letters.length,
+            stagger: {each: 0.05, from: "end"},
+            ease: 'power3.out',
+            marginRight: marginFactor,
+            scale: scaleFactor,
+            fontWeight: 'bold'
+        });
+        // @ts-ignore
+        tl.to(wordRef.current.children, {x: 20, duration: 0.5, scale: 1, marginRight: '5px', border: 0,}, '+=.7');
 
         // Animate the diagonal line
-        tl.to(diagonalLine.current, { autoAlpha: 1, x: 20, duration: 1 }, );
+        tl.to(diagonalLine.current, {autoAlpha: 1, x: 20, duration: 1},);
         // 两行文字和竖线的动画
         // Animate each line separately
-        tl.to(line1.current, { autoAlpha: 1, x: 20, duration: 1 }, '+=0.2')
-            .to(line2.current, { autoAlpha: 1, x: 20, duration: 1 }, '+=0.3');
-
+        tl.to(line1.current, {autoAlpha: 1, x: 20, duration: 1}, '+=0.2')
+            .to(line2.current, {autoAlpha: 1, x: 20, duration: 1}, '+=0.3');
 
 
     }, []);
 
 
     return (
-        <div className="animation-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <div style={{ marginRight: '20px', fontSize: '1.5rem' }}>
+        <div className="animation-container"
+             style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+            <div style={{marginRight: '20px', fontSize: '1.5rem'}}>
                 <div ref={line1}>Furry Visions</div>
                 <div ref={line2}>AI Missions</div>
             </div>
-            <div ref={diagonalLine} style={{ marginRight: '20px', fontSize: '2rem', lineHeight: '0'}}>/</div>
-            <div ref={wordRef} style={{ display: 'flex', flexDirection: 'row',fontSize: '2rem', }}>
+            <div ref={diagonalLine} style={{marginRight: '20px', fontSize: '2rem', lineHeight: '0'}}>/</div>
+            <div ref={wordRef} style={{display: 'flex', flexDirection: 'row', fontSize: '2rem',}}>
                 {Array.from(letters).map((letter, index) => (
                     <p key={index}>{letter}</p>
                 ))}
